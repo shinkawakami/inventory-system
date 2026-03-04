a98cb5381e01610c3ea8103bae48d4dfa5977be3
shinkawakami <snnsk.2002@gmail.com>
2026-03-04 21:46:08 +0900

    productテーブル作成

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE product (
+    product_id SERIAL PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL,
+    price INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
